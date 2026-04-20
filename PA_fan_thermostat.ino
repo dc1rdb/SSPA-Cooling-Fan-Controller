83a5b7fc2c2fcd5f8a99e68a930feee366e5b130
Robert <dc1rdb@users.noreply.github.com>
2026-04-20 06:56:24 +0200

    Refactor thermostat logic and update version
    Updated temperature thresholds and improved sensor handling.

--- PA_fan_thermostat.ino
+++ PA_fan_thermostat.ino
@@ -4,76 +4,65 @@ by measuring the temperature directly at the heat spreader with a DS18B20 temper
 outputting a signal with the Arduino Nano to control the fan of the cooler assembly.
 
 DC1RDB
-Ver. 1.1
-Feb 20, 2026
+Ver. 1.2
+Apr 19, 2026
 */
 
 
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-// Digital pin of temperature sensor
-#define ONE_WIRE_BUS 2  //4.7K pullup resistor reqd
-
-// Setup a oneWire instance
-OneWire oneWire(ONE_WIRE_BUS);  
-
-// Setup temperature sensor library
-DallasTemperature sensors(&oneWire);
-
-// output pin to MOSFET switch
+#define ONE_WIRE_BUS 2
 #define fanPin 10
 
-// how frequently the main loop runs
-const int tempSetInterval = 3000;  //temperature is measured at this interval
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
 
+const unsigned long interval = 3000;
+unsigned long lastUpdate = 0;
 
-// temperature settings
-const float tempLow = 20;  //at and below this temperature the fan shuts off
-const float tempHigh = 24; //at and above this temperature the fan is on
-
+const float tempLow = 25.0;
+const float tempHigh = 26.0;
 
 void setup() {
-	
-	// start serial port 
-	Serial.begin(9600); 
-	
-	// Start up the temperature library 
-	sensors.begin();
-	sensors.requestTemperatures();
+  Serial.begin(9600);
+  sensors.begin();
+  sensors.setWaitForConversion(false); 
 
-	// configure pins
-	pinMode(LED_BUILTIN, OUTPUT);
-	pinMode(fanPin, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(fanPin, OUTPUT);
 
-	// fan power-on test for 2 sec
-	digitalWrite(fanPin, HIGH);
-	digitalWrite(LED_BUILTIN, HIGH);
-  delay (2000);
+  // Self-test
+  digitalWrite(fanPin, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(2000); 
   digitalWrite(fanPin, LOW);
-	digitalWrite(LED_BUILTIN, LOW);
-
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
-
-// main loop ##############################################
 void loop() {
-	
-	sensors.requestTemperatures();
-	float temp = sensors.getTempCByIndex(0);
+  unsigned long currentMillis = millis();
 
-	Serial.print(temp);
-	Serial.print("°C, ");
-	Serial.println(digitalRead (fanPin));
+  if (currentMillis - lastUpdate >= interval) {
+    lastUpdate = currentMillis;
 
-	if (temp >= tempHigh) {
-    digitalWrite(fanPin, HIGH);
-		digitalWrite(LED_BUILTIN, HIGH);
-  } 
-	else if (temp <= tempLow) {
-    digitalWrite(fanPin, LOW);
-		digitalWrite(LED_BUILTIN, LOW);
+    // Request new temp and read the previous one immediately
+    sensors.requestTemperatures();
+    float temp = sensors.getTempCByIndex(0);
+
+    // Filter out sensor errors (-127 occurs if disconnected)
+    if (temp > -50) { 
+      Serial.print(temp);
+      Serial.print("°C, Fan: ");
+      Serial.println(digitalRead(fanPin));
+
+      if (temp >= tempHigh) {
+        digitalWrite(fanPin, HIGH);
+        digitalWrite(LED_BUILTIN, HIGH);
+      } else if (temp <= tempLow) {
+        digitalWrite(fanPin, LOW);
+        digitalWrite(LED_BUILTIN, LOW);
+      }
+    }
   }
-
-	delay(tempSetInterval);
 }
